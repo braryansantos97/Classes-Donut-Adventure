232054a015c5946b0b2ac4ad564b6dbcfef1f63d
Brayan Santos <santos.a.brayan97@gmail.com>
2021-05-17 21:28:16 -0400

    created our enemy pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -21,3 +21,26 @@ class Hero {
 }
 
 const dougie = new Hero('Dougie')
+
+class Enemy {
+  constructor(name) {
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    }
+    this.catchPhrases = ['i\'m youtube famous',
+    'i\'m more dangerous than an uncovered sewer']
+    }
+    talkSmack() {
+      const randomPhraseRat = Math.floor(Math.random()*this.catchPhrases.length);
+    console.log(this.catchPhrases[randomPhraseRat]);
+    }
+    announceHealth() {
+    console.log(this.health);
+  }
+  fight() {
+    console.log('i\'m gonna flatten you like a slice of pepperoni!')
+  }
+}
+const pizzaRat = new Enemy('Pizza Rat')
